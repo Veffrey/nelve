22cc1e9873e1c75fb40d44a1f650d58565743656
veffrey <veffreyokhamera@gmail.com>
2022-01-18 23:07:22 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var navLinks = document.getElementById("navLinks")
+w
+        function showMenu() {
+            navLinks.style.right = "0"
+            
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px"
+        }
+        
+
+var navLinks = document.getElementById("navLinkst")
+w
+        function showMenu() {
+            navLinkst.style.right = "0"
+            
+        }
+        function hideMenu() {
+            navLinkst.style.right = "-200px"
+        }
